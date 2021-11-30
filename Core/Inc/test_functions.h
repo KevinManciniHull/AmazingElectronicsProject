b639fcead211f2f15fb11e0d1629d4850203ad90
Tomáš Krupička <tomas.krupicka770@gmail.com>
2021-11-30 16:27:07 +0000

    reorganisation of the project to fit project structure

---
+++ Core/Inc/test_functions.h
@@ -0,0 +1,13 @@
+/*
+ * test_functions.h
+ *
+ *  Created on: Nov 17, 2021
+ *      Author: kevinmancini
+ */
+
+#ifndef SRC_TEST_FUNCTIONS_H_
+#define SRC_TEST_FUNCTIONS_H_
+
+
+
+#endif /* SRC_TEST_FUNCTIONS_H_ */
